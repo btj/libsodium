1b88a19e58951db37b7d7889dc0035d24a0f758f
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2016-01-05 14:49:29 -0800

    Massaged sign.c so that VeriFast's frontend accepts it; now ready to start spec'ing the APIs used.

--- src/libsodium/crypto_sign/ed25519/ref10/sign.c
+++ src/libsodium/crypto_sign/ed25519/ref10/sign.c
@@ -1,29 +1,78 @@
 
-#include <string.h>
+//#include <string.h>
 
-#include "crypto_hash_sha512.h"
-#include "crypto_sign_ed25519.h"
-#include "utils.h"
-#include "../../../crypto_core/curve25519/ref10/curve25519_ref10.h"
+//#include "crypto_hash_sha512.h"
+//#include "crypto_sign_ed25519.h"
+//#include "utils.h"
+//#include "../../../crypto_core/curve25519/ref10/curve25519_ref10.h"
+
+#define NULL 0
+
+typedef int crypto_hash_sha512_state;
+typedef int ge_p3;
+
+void crypto_hash_sha512(char *az, char *sk, unsigned long length);
+    //@ requires true;
+    //@ ensures true;
+
+void crypto_hash_sha512_init(crypto_hash_sha512_state *hs);
+    //@ requires true;
+    //@ ensures true;
+
+void crypto_hash_sha512_update(crypto_hash_sha512_state *hs, char *az, unsigned long length);
+    //@ requires true;
+    //@ ensures true;
+
+void crypto_hash_sha512_final(crypto_hash_sha512_state *hs, char *nonce);
+    //@ requires true;
+    //@ ensures true;
+
+void memmove(void *to, void *from, unsigned long size);
+    //@ requires true;
+    //@ ensures true;
+
+void sc_reduce(char *nonce);
+    //@ requires true;
+    //@ ensures true;
+
+void ge_scalarmult_base(ge_p3 *R, char *nonce);
+    //@ requires true;
+    //@ ensures true;
+
+void ge_p3_tobytes(char *sig, ge_p3 *R);
+    //@ requires true;
+    //@ ensures true;
+
+void sc_muladd(char *sig, char *hram, char *az, char *nonce);
+    //@ requires true;
+    //@ ensures true;
+
+void sodium_memzero(char *buf, unsigned long size);
+    //@ requires true;
+    //@ ensures true;
+
+//@ predicate integer_opt(int *p;) = p == 0 ? true : integer(p, _);
 
 int
-crypto_sign_ed25519_detached(unsigned char *sig, unsigned long long *siglen_p,
-                             const unsigned char *m, unsigned long long mlen,
-                             const unsigned char *sk)
+crypto_sign_ed25519_detached(char *sig, /*unsigned long long*/ int *siglen_p,
+                             /*const*/ char *m, unsigned /*long*/ long mlen,
+                             /*const*/ char *sk)
+    //@ requires integer_opt(siglen_p);
+    //@ ensures siglen_p == 0 ? true : integer(siglen_p, 64);
 {
     crypto_hash_sha512_state hs;
-    unsigned char az[64];
-    unsigned char nonce[64];
-    unsigned char hram[64];
+    char az[64];
+    char nonce[64];
+    char hram[64];
     ge_p3 R;
 
     crypto_hash_sha512(az, sk, 32);
-    az[0] &= 248;
-    az[31] &= 63;
-    az[31] |= 64;
+    az[0] = (char)(az[0] & 248);
+    az[31] = (char)(az[31] & 63);
+    az[31] = (char)(az[31] | 64);
 
     crypto_hash_sha512_init(&hs);
-    crypto_hash_sha512_update(&hs, az + 32, 32);
+    crypto_hash_sha512_update(&hs, (char *)az + 32, 32);
     crypto_hash_sha512_update(&hs, m, mlen);
     crypto_hash_sha512_final(&hs, nonce);
 
@@ -41,20 +90,20 @@ crypto_sign_ed25519_detached(unsigned char *sig, unsigned long long *siglen_p,
     sc_reduce(hram);
     sc_muladd(sig + 32, hram, az, nonce);
 
-    sodium_memzero(az, sizeof az);
+    sodium_memzero(az, 64);
 
     if (siglen_p != NULL) {
-        *siglen_p = 64U;
+        *siglen_p = 64;
     }
     return 0;
 }
 
 int
-crypto_sign_ed25519(unsigned char *sm, unsigned long long *smlen_p,
-                    const unsigned char *m, unsigned long long mlen,
-                    const unsigned char *sk)
+crypto_sign_ed25519(unsigned char *sm, unsigned /*long*/ long *smlen_p,
+                    /*const*/ unsigned char *m, unsigned /*long*/ long mlen,
+                    /*const*/ unsigned char *sk)
 {
-    unsigned long long siglen;
+    unsigned /*long*/ long siglen;
 
     memmove(sm + crypto_sign_ed25519_BYTES, m, mlen);
 /* LCOV_EXCL_START */
